8b003d5a26e1c8981889557965a9c9d4344fdd1a
Dries Maes <dm.inbox@outlook.com>
2021-04-19 15:42:00 +0200

    Repos staan klaar

--- SwipeIT/SwipeIT/Services/DeveloperRepo.cs
+++ SwipeIT/SwipeIT/Services/DeveloperRepo.cs
@@ -1,10 +1,36 @@
-﻿using System;
+﻿using SwipeIT.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace SwipeIT.Services
 {
-    class DeveloperRepo
+    public class DeveloperRepo : IDataStore<Developer>
     {
+        public Task<bool> AddItemAsync(Developer item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteItemAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Developer> GetItemAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Developer>> GetItemsAsync(bool forceRefresh = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateItemAsync(Developer item)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
