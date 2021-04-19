2ee0aa0d65274e663b8f2d1c42f36b9e25137112
Dries Maes <dm.inbox@outlook.com>
2021-04-19 14:12:17 +0200

    Add project files.

---
+++ SwipeIT/SwipeIT/Views/LoginPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using SwipeIT.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SwipeIT.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new LoginViewModel();
+        }
+    }
+}
