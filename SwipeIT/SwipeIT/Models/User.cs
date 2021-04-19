23940b55f839abc7e7c456008ec38fdff434c882
JensVanGelder <jens.v.gelder@gmail.com>
2021-04-19 15:43:37 +0200

    Finished base models

--- SwipeIT/SwipeIT/Models/User.cs
+++ SwipeIT/SwipeIT/Models/User.cs
@@ -4,7 +4,14 @@ using System.Text;
 
 namespace SwipeIT.Models
 {
-    internal abstract class User
+    public abstract class User : Account
     {
+        public int ID { get; set; }
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public List<String> MyProperty { get; set; }
+        public int YearsOfExperience { get; set; }
+        public List<Skill> Skills { get; set; }
     }
 }
