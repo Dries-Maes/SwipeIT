bb3df4bf12d98cb4beecfac76b6367d5134d9d07
Dries Maes <dm.inbox@outlook.com>
2021-04-20 10:30:07 +0200

    Start-Settings-incomplete

---
+++ SwipeIT/SwipeIT/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using SwipeIT.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace SwipeIT.ViewModels
+{
+    public class SettingsViewModel : BaseViewModel
+    {
+        public Command<Account> SaveCommand => new Command<Account>(Save);
+
+        private void Save(Account obj)
+        {
+            throw new NotImplementedException(); //Call update method
+        }
+    }
+}
