2e1570f0a503a785fefbc6a533f41a3ea4b79259
Impesoft <MrBlubke@yahoo.com>
2021-04-25 19:05:13 +0200

    user admin page formatting

---
+++ SwipeIT/SwipeIT/ViewModels/UsersAdministrationViewModel.cs
@@ -0,0 +1,53 @@
+﻿using SwipeIT.Models;
+using SwipeIT.Services.Repos;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace SwipeIT.ViewModels
+{
+    internal class UsersAdministrationViewModel : BaseViewModel
+    {
+        private string searchText;
+        private List<User> allUsers;
+
+        public string SearchText
+        {
+            get { return searchText; }
+            set
+            {
+                searchText = value;
+                OnPropertyChanged(nameof(SearchText));
+            }
+        }
+
+        private ObservableCollection<User> allUsersListCollection;
+
+        public ObservableCollection<User> AllUsersListCollection
+        {
+            get { return allUsersListCollection; }
+            set
+            {
+                allUsersListCollection = value;
+                OnPropertyChanged(nameof(AllUsersListCollection));
+            }
+        }
+
+        public UsersAdministrationViewModel()
+        {
+            GetSkills().Wait();
+        }
+
+        private async Task GetSkills()
+        {
+            allUsers = new List<User>();
+            allUsers.AddRange(await DeveloperRepo.GetAllItemsAsync());
+            allUsers.AddRange(await RecruiterRepo.GetAllItemsAsync());
+            AllUsersListCollection = new ObservableCollection<User>(allUsers);
+        }
+    }
+}
